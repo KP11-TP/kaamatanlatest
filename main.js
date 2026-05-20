70f90319b9bfd914b2830d33a7e1c9d1aea95972
KP11-TP <aladinfatinnabila@gmail.com>
2026-05-20 09:20:20 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -12,10 +12,7 @@ const createYoutube = () => {
 
     const onYouTubeIframeAPIReady = () => {
       const player = new YT.Player('player', {
-        videoId: 'P_DxID-63H4',
-        width: 1024,
-        height: 576,
-        playerVars: { playsinline: 1, rel: 0 },
+        videoId: 'Mg7AtuLnDTo',
         events: {
           onReady: () => {
             resolve(player);
@@ -37,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Create a MindARThree instance with the specified container and image target source
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targets/Hinava.mind',
+      imageTargetSrc: '../../assets/targets/Suling.mind',
     });
 
     // Extract the renderer, CSS renderer, scene, CSS scene, and camera from the MindARThree instance
@@ -51,14 +48,11 @@ document.addEventListener('DOMContentLoaded', () => {
     cssAnchor.group.add(obj);
 
     // Define actions when the AR target is found and lost
-    const arDiv = document.querySelector('#ar-div');
     cssAnchor.onTargetFound = () => {
-      if(arDiv) arDiv.style.visibility = 'visible';
-      try { player.playVideo(); } catch(e) { }
+      player.playVideo();
     }
     cssAnchor.onTargetLost = () => {
-      if(arDiv) arDiv.style.visibility = 'hidden';
-      try { player.pauseVideo(); } catch(e) { }
+      player.pauseVideo();
     }
 
     // Start the MindARThree AR experience
