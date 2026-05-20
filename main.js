7d5d65b0e6db28060808d0c0ed4991d56fa50b17
KP11-TP <aladinfatinnabila@gmail.com>
2026-05-20 09:20:56 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const createYoutube = () => {
 
     const onYouTubeIframeAPIReady = () => {
       const player = new YT.Player('player', {
-        videoId: 'Mg7AtuLnDTo',
+        videoId: 'svFbZEhVVDg',
         events: {
           onReady: () => {
             resolve(player);
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Create a MindARThree instance with the specified container and image target source
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: '../../assets/targets/Suling.mind',
+      imageTargetSrc: '../../assets/targets/tarianSumazau.mind',
     });
 
     // Extract the renderer, CSS renderer, scene, CSS scene, and camera from the MindARThree instance
